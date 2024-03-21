c37a06abddac81013504e6dcf76e602b44d20c99
PedroHenrique726 <pedrouser9@gmail.com>
2024-03-21 16:50:47 -0300

    Atualização nos Ids nas atividades 1 e 2

--- 21.3 Atividade 1.sql
+++ 21.3 Atividade 1.sql
@@ -3,23 +3,21 @@ CREATE DATABASE db_generation_game_online;
 USE db_generation_game_online;
 
 CREATE TABLE tb_classes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
     habilidade_preferida VARCHAR(30) NOT NULL,
     arma_preferida VARCHAR(30) NOT NULL,
-    vocacao VARCHAR(20) NOT NULL PRIMARY KEY 
+    vocacao VARCHAR(20) NOT NULL UNIQUE
 );
-
-
 
 CREATE TABLE tb_personagens(
-	força INT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    força INT NOT NULL,
     defesa INT NOT NULL,
-    nome VARCHAR(30) NOT NULL PRIMARY KEY,
+    nome VARCHAR(30) NOT NULL,
     destreza INT NOT NULL,
-    classesvocacao VARCHAR(20)
+    classes_id INT,
+    FOREIGN KEY (classes_id) REFERENCES tb_classes(id)
 );
-
-ALTER TABLE tb_personagens ADD CONSTRAINT fk_personagens_classes
-FOREIGN KEY (classesvocacao) REFERENCES tb_classes(vocacao);
 
 INSERT INTO tb_classes(habilidade_preferida, arma_preferida, vocacao)
 VALUES
@@ -29,17 +27,16 @@ VALUES
 ("magia elemental", "cajado", "Druid"),
 ("ataque a distancia", "lança", "Lanceiro");
 
-
-INSERT INTO tb_personagens(força, defesa, nome, destreza, classesvocacao)
+INSERT INTO tb_personagens(força, defesa, nome, destreza, classes_id)
 VALUES
-(1500, 2000, "Knight Boladao", 2500, "Knight"),
-(2000, 1200, "Lord PAulistinha", 1700, "Knight"),
-(2500, 1000, "Dru Ida", 1800, "Druid"),
-(1700, 2500, "Palla Demoniak", 2000, "Paladino"),
-(3000, 500, "Martin", 2800, "Lanceiro"),
-(1200, 2200, "Mage Mystica", 800, "Mago"),
-(1800, 2800, "Rogue Sombrio", 1500, "Mago"),
-(2200, 1700, "Healer Curador", 2000, "Druid");
+(1500, 2000, "Knight Boladao", 2500, 3),
+(2000, 1200, "Lord PAulistinha", 1700, 3),
+(2500, 1000, "Dru Ida", 1800, 4),
+(1700, 2500, "Palla Demoniak", 2000, 2),
+(3000, 500, "Martin", 2800, 5),
+(1200, 2200, "Mage Mystica", 800, 1),
+(1800, 2800, "Rogue Sombrio", 1500, 1),
+(2200, 1700, "Healer Curador", 2000, 4);
 
 SELECT * FROM tb_personagens WHERE força > 2000;
 
@@ -48,8 +45,8 @@ SELECT * FROM tb_personagens  WHERE defesa BETWEEN 1000 AND 2000;
 SELECT * FROM tb_personagens WHERE nome LIKE '%C%';
 
 SELECT * FROM tb_personagens INNER JOIN tb_classes 
-ON tb_personagens.classesvocacao = tb_classes.vocacao;
+ON tb_personagens.classes_id = tb_classes.id;
 
 SELECT * FROM tb_personagens INNER JOIN tb_classes 
-ON tb_personagens.classesvocacao = tb_classes.vocacao
-WHERE tb_personagens.classesvocacao = 'MAgo';
+ON tb_personagens.classes_id = tb_classes.id
+WHERE tb_classes.id = '2';
--- 21.3 Atividade 2.sql
+++ 21.3 Atividade 2.sql
@@ -3,23 +3,20 @@ CREATE DATABASE db_pizzaria_legal;
 USE db_pizzaria_legal;
 
 CREATE TABLE tb_categorias(
-	tipo VARCHAR(30) NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+	tipo VARCHAR(30) NOT NULL,
     descricao VARCHAR(255) NOT NULL,
     especial BOOLEAN
 );
-
-drop table tb_categorias;
-
 
 CREATE TABLE tb_pizzas(
 	preco DECIMAL(6,2) NOT NULL,
     tamanho VARCHAR(255) NOT NULL,
     nome VARCHAR(255) NOT NULL PRIMARY KEY,
-    tipo VARCHAR(255) NOT NULL,
+    categorias_id INT,
     sabor VARCHAR(255) NOT NULL,
-    FOREIGN KEY (tipo) REFERENCES tb_categorias(tipo)
+    FOREIGN KEY (categorias_id) REFERENCES tb_categorias(id)
 );
-
 
 INSERT INTO tb_categorias (tipo, descricao, especial) 
 VALUES('Tradicional', 'Pizzas clássicas e populares', FALSE),
@@ -28,16 +25,15 @@ VALUES('Tradicional', 'Pizzas clássicas e populares', FALSE),
 ('Especial do Chef', 'Pizzas exclusivas criadas pelo chef', TRUE),
 ('Doces', 'Pizzas com coberturas doces', FALSE);
 
-
-INSERT INTO tb_pizzas (preco, tamanho, nome, tipo, sabor) 
-VALUES(20.00, 'Média', 'Margherita', 'Tradicional', 'Molho de tomate, mussarela, manjericão'),
-(125.00, 'Grande', 'Calabresa', 'Tradicional', 'Molho de tomate, calabresa, cebola, mussarela'),
-(30.00, 'Média', 'Vegetariana', 'Vegetariana', 'Molho de tomate, pimentão, cebola, cogumelos, azeitonas'),
-(90.00, 'Grande', 'Frutos do Mar', 'Frutos do Mar', 'Molho de tomate, camarão, lula, mexilhão, mussarela'),
-(40.00, 'Pequena', 'Especial do Chef', 'Especial do Chef', 'Molho especial do chef, ingredientes exclusivos'),
-(45.00, 'Média', 'Quatro Queijos', 'Tradicional', 'Molho de tomate, mussarela, parmesão, gorgonzola, catupiry'),
-(100.00, 'Grande', 'Banana com Chocolate', 'Doces', 'Banana, chocolate, açúcar'),
-(55.00, 'Média', 'Chocolate com Morango', 'Doces', 'Chocolate, morangos, açúcar');
+INSERT INTO tb_pizzas (preco, tamanho, nome, categorias_id, sabor) 
+VALUES(20.00, 'Média', 'Margherita', 1, 'Molho de tomate, mussarela, manjericão'),
+(125.00, 'Grande', 'Calabresa', 1, 'Molho de tomate, calabresa, cebola, mussarela'),
+(30.00, 'Média', 'Vegetariana', 2, 'Molho de tomate, pimentão, cebola, cogumelos, azeitonas'),
+(90.00, 'Grande', 'Frutos do Mar', 3, 'Molho de tomate, camarão, lula, mexilhão, mussarela'),
+(40.00, 'Pequena', 'Especial do Chef', 4, 'Molho especial do chef, ingredientes exclusivos'),
+(45.00, 'Média', 'Quatro Queijos', 1, 'Molho de tomate, mussarela, parmesão, gorgonzola, catupiry'),
+(100.00, 'Grande', 'Banana com Chocolate', 5, 'Banana, chocolate, açúcar'),
+(55.00, 'Média', 'Chocolate com Morango', 5, 'Chocolate, morangos, açúcar');
 
 SELECT * FROM tb_pizzas WHERE preco > 45.00;
 
@@ -47,10 +43,9 @@ SELECT * FROM tb_pizzas WHERE nome LIKE '%M%';
 
 SELECT * FROM tb_pizzas 
 INNER JOIN tb_categorias
-ON tb_pizzas.tipo = tb_categorias.tipo;
-
+ON tb_pizzas.categorias_id = tb_categorias.id;
 
 SELECT * FROM tb_pizzas 
 INNER JOIN tb_categorias
-ON tb_pizzas.tipo = tb_categorias.tipo
-WHERE tb_pizzas.tipo = "Doces";
+ON tb_pizzas.categorias_id = tb_categorias.id
+WHERE tb_categorias.tipo = "Doces";
